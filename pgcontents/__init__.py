545d5b2cb6d6933e18fc6c4d63f752eb9c9ca7b5
Shuai Lin <linshuai2012@gmail.com>
2016-05-16 14:21:45 +0800

    Add an alias class to avoid confusion.

--- pgcontents/__init__.py
+++ pgcontents/__init__.py
@@ -1,7 +1,11 @@
 from .checkpoints import PostgresCheckpoints
 from .pgmanager import PostgresContentsManager
 
+class MySQLContentsManager(PostgresContentsManager):
+    pass
+
 __all__ = [
     'PostgresCheckpoints',
     'PostgresContentsManager',
+    'MySQLContentsManager',
 ]
