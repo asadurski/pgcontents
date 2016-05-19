78bd12788a1921df5a320773126ec311c38656fd
Shuai Lin <linshuai2012@gmail.com>
2016-05-19 15:47:03 +0800

    Set pool_recycle for sqlalchemy engine.

--- pgcontents/managerbase.py
+++ pgcontents/managerbase.py
@@ -58,7 +58,10 @@ class PostgresManagerMixin(HasTraits):
     engine = Instance(Engine)
 
     def _engine_default(self):
-        return create_engine(self.db_url, echo=False)
+        engine_settings = {
+            'pool_recycle': 3600,
+        }
+        return create_engine(self.db_url, echo=False, **engine_settings)
 
     def __init__(self, *args, **kwargs):
         super(PostgresManagerMixin, self).__init__(*args, **kwargs)
